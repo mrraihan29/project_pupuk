9c7cbb32644a5c5a87cde17ef2648d5f222dc921
mrraihan29 <mrraihan2002@gmail.com>
2025-12-30 02:12:06 +0700

    feat: Disable strict mode for Whitenoise manifest to improve static file handling

--- sim_dp/settings.py
+++ sim_dp/settings.py
@@ -132,7 +132,7 @@ SESSION_COOKIE_SAMESITE = os.getenv('SESSION_COOKIE_SAMESITE', 'Lax')
 
 # Konfigurasi Static Files (CSS, JS)
 STATIC_URL = 'static/'
-
+WHITENOISE_MANIFEST_STRICT = False
 # Folder tempat mengumpulkan static files saat deploy
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
